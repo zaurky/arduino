ff02c61b104c83762de6d1c56cbfdc8c923e7553
zaurky <zaurky@zeb.re>
2013-05-26 11:48:05 +0200

    use LedInit get serial messages and give them to Alive::order

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -10,8 +10,9 @@ get 32 bits values.
 
 
 // arduino pin where things are plugged
-int msg_id = 2;
-int alive_id = 4;
+int leds[3] = {2, 4, 5};
+int msg_id = leds[1];
+int alive_id = leds[0];
 int rc_irq = 0;  // Receiver on inerrupt 0 => that is pin #3 on leonardo
 
 
@@ -23,18 +24,44 @@ Sensor sensor = Sensor(msg_id);
 
 void setup() {
     mySwitch.enableReceive(rc_irq);
+    LedInit init = LedInit(leds, 3);
+    init.init();
     sensor.init();
     alive.init();
 }
 
 
+void readSerialString(char* serInString) {
+    int sb;
+    int serInIndx = 0;
+    if (Serial.available()) {
+        while (Serial.available()){
+            sb = Serial.read();
+            serInString[serInIndx] = sb;
+            serInIndx++;
+        }
+    }
+}
+
+
 void loop() {
+    // if something comes from the RF sensor
     if (mySwitch.available()) {
         sensor.work(mySwitch.getReceivedValue());
         mySwitch.resetAvailable();
     }
 
+    // if something comes from the serial
+    if (Serial.available() > 0) {
+        char serInString[50];
+        readSerialString(serInString);
+        alive.order(serInString);
+    }
+
     // do checks
     sensor.check();
     alive.check();
+
+    // wait between loops
+    delay(100);
 }
