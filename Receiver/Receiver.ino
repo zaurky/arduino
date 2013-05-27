4129fbc8817bf8e4760d7a0116cc66ff3ee3e636
zaurky <zaurky@zeb.re>
2013-05-27 17:40:26 +0200

    add an "armed" led that get on when sensor type key_on is pressed, and off when key_off

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -9,10 +9,11 @@ get 32 bits values.
 */
 
 
-// arduino pin where things are plugged
+// arduino pin where things are plugged (need at least 3 leds)
 int leds[3] = {2, 4, 5};
 int msg_id = leds[1];
 int alive_id = leds[0];
+int armed_id = leds[2];
 int rc_irq = 0;  // Receiver on inerrupt 0 => that is pin #3 on leonardo
 
 
@@ -20,6 +21,7 @@ int rc_irq = 0;  // Receiver on inerrupt 0 => that is pin #3 on leonardo
 RCSwitch mySwitch = RCSwitch();
 Alive alive = Alive(alive_id);
 Sensor sensor = Sensor(msg_id);
+// Blink2 armed = Blink2(armed_id);
 
 
 void setup() {
@@ -45,10 +47,18 @@ void readSerialString(char* serInString) {
 
 
 void loop() {
+    int sensor_type;
     // if something comes from the RF sensor
     if (mySwitch.available()) {
-        sensor.work(mySwitch.getReceivedValue());
+        sensor_type = sensor.work(mySwitch.getReceivedValue());
         mySwitch.resetAvailable();
+        if (sensor_type == sensor_type_key_on) {
+            digitalWrite(armed_id, HIGH);
+            Serial.println("Activating system");
+        } else if (sensor_type == sensor_type_key_off) {
+            digitalWrite(armed_id, LOW);
+            Serial.println("Deactivating system");
+        }
     }
 
     // if something comes from the serial
@@ -63,5 +73,5 @@ void loop() {
     alive.check();
 
     // wait between loops
-    delay(100);
+    delay(10);
 }
