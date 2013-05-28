3d9915346fd02b2e301180b970b5d34353a600e7
zaurky <zaurky@zeb.re>
2013-05-28 11:37:36 +0200

    coherant reindent (4 spaces)

--- libraries/blink2/examples/_3leds_blinks/_3leds_blinks.ino
+++ libraries/blink2/examples/_3leds_blinks/_3leds_blinks.ino
@@ -4,17 +4,17 @@ boolean done[] = {true, true, true};
 Blink2 blink[] = {Blink2(2, 1000), Blink2(3, 2000), Blink2(4, 3000)};
 
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop() {
-  for (int index = 0; index < 3; index++) {
-    if (done[index]) {
-      Serial.print("reinit : ");
-      Serial.println(index);
-      blink[index].blink(5*(index+1));
+    for (int index = 0; index < 3; index++) {
+        if (done[index]) {
+            Serial.print("reinit : ");
+            Serial.println(index);
+            blink[index].blink(5*(index+1));
+        }
+        done[index] = blink[index].check();
     }
-    done[index] = blink[index].check();
-  }
-  delay(10);
+    delay(10);
 }
