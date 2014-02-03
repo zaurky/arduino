6d3a2c9ec1a990edb589c60dbf9255237a3e7111
zaurky <zaurky@zeb.re>
2014-02-03 16:57:29 +0100

    build in a separate dir

--- build.sh
+++ build.sh
@@ -3,10 +3,13 @@
 PORT="/dev/ttyACM0"
 PROJECT="Transmitter"
 LIBS="blink2 RCSwitch buzzer sensor alarm utils serialconsole transmitter"
+BUILD="build"
+
+mkdir -p "$BUILD/libs/"
 
 # convert ino sketch file into a .cpp
-echo -e '#include "Arduino.h"\n\n' > "$PROJECT.cpp"
-cat "$PROJECT/$PROJECT.ino" >> "$PROJECT.cpp"
+echo -e '#include "Arduino.h"\n\n' > "$BUILD/$PROJECT.cpp"
+cat "$PROJECT/$PROJECT.ino" >> "$BUILD/$PROJECT.cpp"
 
 
 LIB_INCLUDE="-I ../../arduino/ -I ../`echo $LIBS | sed -e 's/ / -I ..\//'g`"
@@ -17,19 +20,23 @@ OPTIONS="-c -g -Os -w -Wl,--gc-sections
 
 for lib in $LIBS; do
     cd libraries/${lib}
-    avr-g++ $OPTIONS $LIB_INCLUDE -o ${lib}.o ${lib}.cpp
+    avr-g++ $OPTIONS $LIB_INCLUDE -o ../../$BUILD/libs/${lib}.o ${lib}.cpp
     cd ../..
 done
 
-LIB_INCLUDE="-I arduino/ -I libraries/`echo $LIBS | sed -e 's/ / -I libraries\//'g`"
-avr-g++ $OPTIONS $LIB_INCLUDE -o "$PROJECT.o" "$PROJECT.cpp"
+LIB_INCLUDE="-I libraries/$(echo $LIBS | sed -e 's/ / -I libraries\//g')"
+LIB_INCLUDE="-I arduino/ $LIB_INCLUDE"
+avr-g++ $OPTIONS $LIB_INCLUDE -o "$BUILD/$PROJECT.o" "$BUILD/$PROJECT.cpp"
 
-avr-gcc -Os -Wl,--gc-sections -mmcu=atmega32u4 -o "$PROJECT.elf" "$PROJECT.o" libraries/*/*.o  core/core.a -L core -lm
+avr-gcc -Os -Wl,--gc-sections -mmcu=atmega32u4 \
+    -o "$BUILD/$PROJECT.elf" "$BUILD/$PROJECT.o" \
+    "$BUILD/libs/"*.o core/core.a -L core -lm
 
-avr-objcopy -O ihex -R .eeprom "$PROJECT.elf" "$PROJECT.hex"
+avr-objcopy -O ihex -R .eeprom "$BUILD/$PROJECT.elf" "$BUILD/$PROJECT.hex"
 
 ./utils/reset.py "$PORT"
 
 sleep 2
 
-avrdude -patmega32u4 -cavr109 -P "$PORT" -b57600 -D -U "flash:w:$PROJECT.hex"
+avrdude -patmega32u4 -cavr109 -P "$PORT" -b57600 \
+    -D -U "flash:w:$BUILD/$PROJECT.hex"
