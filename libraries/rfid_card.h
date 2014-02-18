3278cf70922280632333766d0224851ef76aa6b6
zaurky <zaurky@zeb.re>
2014-02-18 17:23:42 +0100

    typos

--- libraries/rfid_card.h
+++ libraries/rfid_card.h
@@ -1,10 +1,10 @@
-#ifndef rfid_constants_h
-#define rfid_constants_h
+#ifndef rfid_card_h
+#define rfid_card_h
 
 
 #define sensor_rfid0 0xa306408b // [A3, 6, 40, 8B]
 #define sensor_rfid1 0x93367f81 // [93, 36, 7F, 81]
-#define sensot_rfid2 0x8e49dc84 // [8E, 49, DC, 84]
+#define sensor_rfid2 0x8e49dc84 // [8E, 49, DC, 84]
 
 
 class Card {
