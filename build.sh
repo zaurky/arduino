723058d7a54dcaa2908c0b43216f60db40c2e1d4
zaurky <zaurky@zeb.re>
2014-02-19 16:20:24 +0100

    update build.sh even if no longer used

--- build.sh
+++ build.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 PORT="/dev/ttyACM0"
-PROJECT="Transmitter"
-LIBS="blink2 RCSwitch buzzer sensor alarm utils serialconsole transmitter"
+PROJECT="dump_rfid"
+LIBS="blink2 RCSwitch buzzer sensor alarm utils serialconsole transmitter MFRC522 SIP"
 BUILD="build"
 
 mkdir -p "$BUILD/libs/"
 
 # convert ino sketch file into a .cpp
 echo -e '#include "Arduino.h"\n\n' > "$BUILD/$PROJECT.cpp"
-cat "$PROJECT/$PROJECT.ino" >> "$BUILD/$PROJECT.cpp"
+cat "project/$PROJECT.ino" >> "$BUILD/$PROJECT.cpp"
 
 
-LIB_INCLUDE="-I ../../arduino/ -I ../`echo $LIBS | sed -e 's/ / -I ..\//'g`"
+LIB_INCLUDE="-I ../arduino/ -I `echo $LIBS | sed -e 's/ / -I /'g`"
 
 OPTIONS="-c -g -Os -w -Wl,--gc-sections
     -fno-exceptions -ffunction-sections -fdata-sections
     -mmcu=atmega32u4 -DF_CPU=16000000L -DARDUINO=22"
 
+cd libraries/
 for lib in $LIBS; do
-    cd libraries/${lib}
-    avr-g++ $OPTIONS $LIB_INCLUDE -o ../../$BUILD/libs/${lib}.o ${lib}.cpp
-    cd ../..
+    avr-g++ $OPTIONS $LIB_INCLUDE -o ../$BUILD/libs/${lib}.o ${lib}.cpp
 done
+cd ..
 
 LIB_INCLUDE="-I libraries/$(echo $LIBS | sed -e 's/ / -I libraries\//g')"
 LIB_INCLUDE="-I arduino/ $LIB_INCLUDE"
